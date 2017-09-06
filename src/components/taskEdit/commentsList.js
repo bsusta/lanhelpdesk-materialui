b325151b979e78923984e536c9fe639ff04a08d9
bsusta <susta@lansystems.sk>
2017-09-06 15:53:28 +0200

    BS-comments-rozpracovane

--- src/components/taskEdit/commentsList.js
+++ src/components/taskEdit/commentsList.js
@@ -12,47 +12,35 @@ import {
 import FlatButton from "material-ui/FlatButton";
 import TextField from "material-ui/TextField";
 import { Card, CardActions, CardHeader, CardText } from "material-ui/Card";
-import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
-import Avatar from 'material-ui/Avatar';
-import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import { List, ListItem } from "material-ui/List";
+import Divider from "material-ui/Divider";
+import Subheader from "material-ui/Subheader";
+import Avatar from "material-ui/Avatar";
+import { grey400, darkBlack, lightBlack } from "material-ui/styles/colors";
 
 class CommentsList extends Component {
   render() {
     return (
-      <div style={{marginTop:10}}>
+      <div style={{ marginTop: 10 }}>
         <List>
-     <Subheader>Today</Subheader>
-     <ListItem
-       leftAvatar={<Avatar src="images/ok-128.jpg" />}
-       primaryText="Brunch this weekend?"
-       secondaryText={
-         <p>
-           <span style={{color: darkBlack}}>Brendan Lim</span> --
-           I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-         </p>
-       }
-       secondaryTextLines={2}
-     />
-     <Divider inset={true} />
-     <ListItem
-       leftAvatar={<Avatar src="images/kolage-128.jpg" />}
-       primaryText={
-         <p>Summer BBQ&nbsp;&nbsp;<span style={{color: lightBlack}}>4</span></p>
-       }
-       secondaryText={
-         <p>
-           <span style={{color: darkBlack}}>to me, Scott, Jennifer</span> --
-           Wish I could come, but I&apos;m out of town this weekend.
-         </p>
-       }
-       secondaryTextLines={2}
-     />
-     <Divider inset={true} />
-
-
-   </List>
+          <ListItem
+            primaryText={
+              <div>
+                <span>Admin</span>
+                <span style={{fontStyle:"italic"}}> posted a note</span>
+                <span style={{float:"right"}}> 8:00 31.5.2017</span>
+              </div>
+          }
+            secondaryText={
+              <p>
+                I&apos;ll be in your neighborhood doing errands this weekend. Do
+                you want to grab brunch?
+              </p>
+            }
+            secondaryTextLines={2}
+          />
+          <Divider />
+        </List>
       </div>
     );
   }
